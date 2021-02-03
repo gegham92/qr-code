2caf29acda1389af85d73bc7328ca22cfa82f0e8
v.ivanidi <v.ivanidi@msforyou.ru>
2021-02-03 14:51:22 +0000

    BarcodeReadDemo_python.py edited online with Bitbucket

--- python/BarcodeReadDemo_python.py
+++ python/BarcodeReadDemo_python.py
@@ -1,7 +1,7 @@
 import sys
 from dbr import *
 
-license_key = 't0077xQAAADfM4dA6rhDHG960qSZ1K5lsh+LaiqSxeTBV3hTqqGgJ+mZyDYGWk6oGTVOoE3zKECg5c2JWr+Ga4zxiCj1s/gVkdx/jAQfzKWs='
+license_key = 'bSs148sOk7FdABUGnxIr7QItQ7nfJjKREFFK16z3SIMbPy+Hk1Q9xdainmD2Jd6iBMguAV6Hge2uzAI5I5qCeMLXBNXDJIIIVFayth1g0a8nBeM1C1zcKBpCpc/wT88mr5lx6c5c6F9CtFw5d/0iDMVZDF4lQNV1vCL9paiv8Aldved9Q20R9HX9OQgzQGv6StmmPtsFqkzuMP9FjGK+3EgnW99cL28jRuxk7BBJT8WHqW4zUIPzI+p+LMtMqjluj7uV+rwevzvY+5kwXJy5fYSXLOd+'
 image = sys.argv[1]
 
 reader = BarcodeReader()
